f76b0ee57e440c9827ae258d8f21eae904163021
camilvs <urenacamila0@gmail.com>
2025-03-31 13:24:13 -0400

    changed style

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
 
 app = Flask(__name__)
-conn_str = "mysql://root:iit123@localhost/boatdb"
+conn_str = "mysql://root:CSET155@localhost/boatdb"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
@@ -46,6 +46,7 @@ def create_boat():
             text("INSERT INTO boats values (:id, :name, :type, :owner_id, :rental_price)"),
             request.form
         )
+        conn.commit()
         return render_template('boats_create.html', error=None, success="Data inserted successfully!")
     except Exception as e:
         error = e.orig.args[1]
